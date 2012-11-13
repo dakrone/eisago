531801a39a48e6eb725c39bac2f45fe1e3fd04f0
Lee Hinman <lee@writequit.org>
2012-11-13 12:14:22 -0500

    move the connection pooling so the lein command uses it

--- src/eisago/import.clj
+++ src/eisago/import.clj
@@ -20,20 +20,20 @@
   "Given the filename of a gzipped json file containing the metadata,
   index all the metadata into ES."
   [filename]
-  (let [data (read-json-file filename)]
-    (when-not (es/index-exists?)
-      (es/create-index es/es-index))
-    (es/index-project data)
-    (es/index-all-vars data)))
+  (http/with-connection-pool {}
+    (let [data (read-json-file filename)]
+      (when-not (es/index-exists?)
+        (es/create-index es/es-index))
+      (es/index-project data)
+      (es/index-all-vars data))))
 
 (defn- test-it [& [filename]]
   ;; connection-pool brings it from ~12 seconds to ~4.5
-  (http/with-connection-pool {}
-    (index-everything filename)
-    (es/add-example "e194bda67affc5915db2460bd4bc29a6"
-                    "This is an example for reduce.")
-    (es/add-example "e194bda67affc5915db2460bd4bc29a6"
-                    "This is a different example for reduce.")
-    (es/add-comment "e194bda67affc5915db2460bd4bc29a6"
-                    "This is a comment for reduce.")
-    (println "done.")))
+  (index-everything filename)
+  (es/add-example "e194bda67affc5915db2460bd4bc29a6"
+                  "This is an example for reduce.")
+  (es/add-example "e194bda67affc5915db2460bd4bc29a6"
+                  "This is a different example for reduce.")
+  (es/add-comment "e194bda67affc5915db2460bd4bc29a6"
+                  "This is a comment for reduce.")
+  (println "done."))
