c22c2def060356b43b24ddb9ee59b4a8875f9bbb
Lee Hinman <lee@writequit.org>
2012-11-15 09:21:00 -0500

    add comment migrations

--- src/eisago/migrate.clj
+++ src/eisago/migrate.clj
@@ -3,7 +3,9 @@
             [clojure.java.jdbc :as sql]
             [eisago.es :as es]))
 
-;; migrate existing clojuredocs examples to eisago
+;; migrate existing clojuredocs examples to eisago, this is only
+;; intended to be run by me (Lee) and from a REPL, since hopefully you
+;; don't have to do it more than once.
 
 (def mysql-db {:subprotocol "mysql"
                :subname "//127.0.0.1:3306/clojuredocs_production"
@@ -16,6 +18,23 @@
                        (:ns m)
                        (:function m))]
       (es/add-example id (:body m)))))
+
+(defn add-comment
+  [m]
+  (when (= (:library m) "Clojure Core")
+    (let [id (es/id-of "clojure"
+                       (:ns m)
+                       (:function m))]
+      (es/add-comment id (:body m)))))
+
+(defn add-all-comments
+  []
+  (http/with-connection-pool {}
+    (sql/with-connection mysql-db
+      (sql/with-query-results rows
+        ["SELECT * FROM flat_comments_view"]
+        (println "Added" (count (doall (map add-comment rows)))
+                 "comments.")))))
 
 (defn add-all-examples
   []
